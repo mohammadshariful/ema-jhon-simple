27499b3705ec1a6172afc5f2a5d3fd2fe853bb8c
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-03-30 23:51:37 +0600

    added dynamic functionality

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { removeFromDb } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
 import useCart from "../Hook/useCart";
 import useProducts from "../Hook/useProducts";
+import ReviewItem from "../ReviewItem/ReviewItem";
 import "./Orders.css";
 const Orders = () => {
   const [products, setProducts] = useProducts();
   const [cart, setCart] = useCart(products);
+  const handlerRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd.id !== product.id);
+    setCart(rest);
+    removeFromDb(product.id);
+  };
   return (
-    <div>
-      <h1>This is orders component</h1>
-      <h1>Orders items {products.length}</h1>
-      <h1>{cart.length}</h1>
+    <div className="shop-container">
+      <div className="review-items-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handlerRemoveProduct={handlerRemoveProduct}
+          />
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart} />
+      </div>
     </div>
   );
 };
